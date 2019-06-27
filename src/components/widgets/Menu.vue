39e7772f09766ab784c0ecbca7f9502d8f96bb1f
LaurentGoweb <la@goweb.fr>
2019-06-27 11:17:04 +0200

    MAJ Correction

--- src/components/widgets/Menu.vue
+++ src/components/widgets/Menu.vue
@@ -2,7 +2,9 @@
  <nav>
     <ul class="d-flex flex-column">
       <li><router-link to="/">Accueil</router-link></li>
-      <li><router-link to="/Demo">Démos</router-link></li>
+      <li><router-link to="/Page1">Page1</router-link></li>
+      <li><router-link to="/Page2">Page2</router-link></li>
+
     </ul>
    
   </nav>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 // import Dropdown from 'bp-vuejs-dropdown'
 import Welcome from './components/Welcome.vue'
+import Page1 from './components/Page1.vue'
+import Page2 from './components/Page2.vue'
 
 // Vue.use(Dropdown)
 Vue.use(VueRouter)
 const routes = [
-  { path : '/' , component : Welcome}
+  { path : '/' , component : Welcome},
+  { path : '/Page1' , component : Page1},
+  { path : '/Page2' , component : Page2}
+
+
 ]
 
 const router = new VueRouter({
